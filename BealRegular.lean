c08d7e2aaef97bbbdc6cb1f38151488005e15c95
Mark Rich <mark.rich388@gmail.com>
2026-07-16 22:43:02 -0700

    Formalize the analytic class number bridge at 23 (#9)

--- BealRegular.lean
+++ BealRegular.lean
@@ -19,6 +19,7 @@ import BealRegular.TwentyThreeRealSubfieldPID
 import BealRegular.TwentyThreeHasseUnitIndex
 import BealRegular.RelativeClassGroup
 import BealRegular.TwentyThreeRelativeClassNumberResultant
+import BealRegular.TwentyThreeAnalyticClassNumber
 import BealRegular.TwentyThreeRelativeClassNumber
 import FltRegular.SmallNumbers.SmallNumbers
 
