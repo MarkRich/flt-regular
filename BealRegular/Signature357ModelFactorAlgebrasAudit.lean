0fedb35a2da4c2289779499d515fb710843bc855
Mark Rich <mark.rich388@gmail.com>
2026-07-18 00:35:09 -0700

    feat: package signature 357 model factor algebras (#40)

---
+++ BealRegular/Signature357ModelFactorAlgebrasAudit.lean
@@ -0,0 +1,14 @@
+import BealRegular.Signature357ModelFactorAlgebras
+
+/-! Kernel axiom audit for the signature `(3,5,7)` model-factor algebras. -/
+
+namespace BealRegular.Signature357ModelFactorAlgebras
+
+#print axioms quadraticFactorAlgebra_finrank
+#print axioms quarticFactorAlgebra_finrank
+#print axioms binomialModelAlgebra_finite_finrank
+#print axioms quadraticFactorAlgebra_finiteEtale
+#print axioms quarticFactorAlgebra_finiteEtale
+#print axioms binomialModelAlgebra_finiteEtale
+
+end BealRegular.Signature357ModelFactorAlgebras
